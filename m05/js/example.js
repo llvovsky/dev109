3d638a2fa3bc3366956e4dda32571ee1811647fa
llvovsky <lvovsky.lev@gmail.com>
2025-05-06 07:42:03 -0700

    Update example.js

--- m05/js/example.js
+++ m05/js/example.js
@@ -1,20 +1,22 @@
 var i = 1; // Set counter to 1  
-// Message
 var msg = '<h2>Multiplication Table</h2>';
+
 // Prompt to enter number from 0 - 10
-var input  = prompt("Please enter a number from 0 - 10");
-  
-//Validation if a number is a null or NaN response as 0
-if(isNaN(input) || input === null || input === '')
-{
-  input = 0;
+var input = prompt("Please enter a number from 0 - 10");
+
+// Convert input to a number
+var number = parseInt(input, 10);
+
+// Validation if input is null, empty, or not a valid number set default to 0
+if (isNaN(number) || input === null || input.trim() === '') {
+    number = 0;
 }
 
-//Loop to calculate the Multiplication table 
+// Loop to calculate the Multiplication table 
 while (i < 11) {
-  msg += i + ' x ' + input + ' = ' + (i * input) + '<br>';
-  i++;
+    msg += i + ' x ' + number + ' = ' + (i * number) + '<br>';
+    i++;
 }
+
 // Write the message into the page
 document.getElementById('blackboard').innerHTML = msg;
-
