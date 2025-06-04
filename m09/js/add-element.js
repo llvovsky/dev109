38b3fee34fec5a01c54dab09883a5ab0557d7311
llvovsky <lvovsky.lev@gmail.com>
2025-06-04 12:43:31 -0700

    Update add-element.js

--- m09/js/add-element.js
+++ m09/js/add-element.js
@@ -1,1 +1,14 @@
-1
+function addItem() {
+      // Get the input 
+      var itemText = document.getElementById("newGroceryItem").value;
+
+      // Create a new grocery item
+      var li = document.createElement("li");
+      li.textContent = itemText;
+
+      // Add a new grocery item to the list
+      document.getElementById("todo").appendChild(li);
+
+      // Clear the input box
+      document.getElementById("newGroceryItem").value = "";
+    }
