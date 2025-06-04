0ca874993d758c56aad92425206deb64bf972136
llvovsky <lvovsky.lev@gmail.com>
2025-06-04 13:24:06 -0700

    Update add-element.js

--- m09/js/add-element.js
+++ m09/js/add-element.js
@@ -1,6 +1,14 @@
 function addItem() {
+      'use strict';
+      
+      try {
       // Get the input 
       var itemText = document.getElementById("newGroceryItem").value;
+
+      // Check for null or empty string
+       if (!itemText.trim()) {
+       throw new Error("Entry is empty or null");
+       }
 
       // Create a new grocery item
       var li = document.createElement("li");
@@ -8,7 +16,20 @@ function addItem() {
 
       // Add a new grocery item to the list
       document.getElementById("todo").appendChild(li);
+      }
+      catch (e) {          
+       console.log(e);  // Log error to console
+       return;          // Stop execution here if error occurredor break if inside a loop
+      } 
+            
+    finally {
+    // Show message in page (not popup)
+    var messageArea = document.getElementById("message");
+    if (messageArea) {
+      messageArea.textContent = "If entry was empty, no item was added.";
+    }
 
       // Clear the input box
       document.getElementById("newGroceryItem").value = "";
     }
+}
